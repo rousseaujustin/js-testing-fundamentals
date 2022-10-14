ce3f53beb9c7252cc175929cab1e26bcb35b5e7a
Justin Rousseau <jrousseau@bestpass.com>
2022-10-14 13:09:52 -0400

    feat: improve with test utilities with more helpful error message and running all of the tests in the file

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,15 +1,35 @@
 const {sum, subtract} = require('../math')
 
-let result, expected
+test('sum adds numbers', () => {
+  const result = sum(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+})
 
-result = sum(3, 7)
-expected = 10
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+test('subtract subtracts numbers', () => {
+  const result = subtract(7, 3)
+  const expected = 4
+  expect(result).toBe(expected);
+})
+
+
+function test(title, callback) {
+  try {
+    callback()
+    console.log(`✔ ${title}`)
+  } catch (error) {
+    console.error(`❌ ${title}`)
+    console.error(error)
+  }
 }
 
-result = subtract(7, 3)
-expected = 4
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+function expect(actual){
+  return {
+    toBe(expected){
+      if (actual !== expected)
+      throw new Error(`${actual} is not equal to ${expected}`)
+    },
+    // toEqual(expected) {},
+    // toBeGreaterThan(expected) {},
+  }
 }
